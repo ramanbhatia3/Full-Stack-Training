ac659efb97ecfd70a13f8dd61c7b6c84bf81a924
ramanbhatia3 <bhattiramandeep3105@gmail.com>
2025-07-07 22:15:11 +0530

    update

---
+++ Day_6/Class_Work/index.js
@@ -0,0 +1,106 @@
+// console.log(document.getElementById(heading))
+        document.getElementById("heading").innerText="Hi from Script";
+        document.getElementById("heading").style.border="2px solid red";
+        document.getElementById("heading").style.width="50%";
+        document.getElementById("heading").style.padding="20%";
+        document.getElementById("heading").style.margin="auto";
+        document.getElementById("heading").style.textAlign="center";
+
+
+        console.log(window)
+        console.log(document)
+        console.log("R B")
+
+// Variables
+
+// let num = 1;
+// const num1 = 2;
+// var num2 = 3;
+
+// console.log(num)
+// console.log(num1)
+// console.log(num2)
+
+// let num = 2;
+// const num2 = 3;
+
+// Functions
+
+// TRADITIONAL FUNCTION
+// function myFunction(){
+//     let a = 10;
+//     let b = 40;
+    
+    
+//     console.log(a+b)
+// }
+
+
+// myFunction()
+
+
+// ARROW FUNCTION 
+
+// const myFunction=()=>{
+//     let a = 10;
+//     let b = 40;
+    
+    
+//     console.log(a+b)
+// }
+
+// myFunction()
+
+// var name = "Thanos";
+
+// console.log(name);
+
+// console.log(url)
+
+// var url = "https://www.google.com/"
+
+// console.log(url)
+
+// console.log(value)
+// console.log(url)
+
+// var url = 'google.com';
+// myFun()
+// console.log(url);
+// const value = 90;
+// console.log(value)
+
+// function myFun(){
+//     let a = 5;
+//     let b = 7;
+//     console.log(a+b);
+// }
+
+// console.log(url);
+// myFun()
+
+// function myFun(x,y){
+//     console.log(x+" "+y);
+// }
+
+// let a = "Rayat"
+// let b = "Bahra"
+// myFun(a,b);
+
+// function myFun(){
+//     let a = "Rayat"
+//     let b = "Bahra"
+//     function nestedFun(){
+//         console.log(a,b);
+//     }
+// }
+
+let name = "Rayat Bahra";
+console.log(name.length);
+
+let firstCharacter = name[0];
+console.log(firstCharacter);
+
+let lastCharacter = name[length-1];
+console.log(lastCharacter);
+
